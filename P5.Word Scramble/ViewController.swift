860303286d1b5f1e110576771b55bb54ed3bcd5b
partalov <partalov@hotmail.com>
2021-09-16 13:31:23 +0200

    Initial Commit

---
+++ P5.Word Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P5.Word Scramble
+//
+//  Created by Ilija Partalov on 16.9.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
